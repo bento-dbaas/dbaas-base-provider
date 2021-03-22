15aa28329d10a2687a659ef1929bbb49e2705882
William Marquardt <william.marquardt@g.globo>
2021-03-22 10:24:02 -0300

    Wait methods in BaseProvider

--- base_provider/base.py
+++ base_provider/base.py
@@ -1,13 +1,9 @@
 from pymongo import MongoClient, ReturnDocument
 
-
 class BaseProviderObject(object):
-    credential = None
-    client = None
-
     def __init__(self, *args, **kwargs):
         pass
-
+    
 
 class BaseProvider(BaseProviderObject):
     def __init__(self, environment, auth_info=None):
@@ -60,6 +56,37 @@ class BaseProvider(BaseProviderObject):
     def provider(self):
         return self.get_provider()
 
+    def wait_zone_operation(self, zone, operation):
+        op = self.client.zoneOperations().wait(
+            project=self.credential.project,
+            zone=zone,
+            operation=operation
+        ).execute()
+        return self._check_operation_status(op)
+
+    def wait_global_operation(self, operation):
+        op = self.client.globalOperations().wait(
+            project=self.credential.project,
+            operation=operation
+        ).execute()
+        self._check_operation_status(op)
+
+    def _check_operation_status(self, operation):
+        if operation.get('error'):
+            error = 'Error in {} operation: {}'.format(
+                operation.get('operationType'),
+                operation.get('error')
+            )
+            raise Exception(error)
+
+        if operation.get('status') != 'DONE':
+            error = 'Operation {} is not Done. Status: {}'.format(
+                operation.get('operationType'),
+                operation.get('status')
+            )
+            raise Exception(error)
+
+        return True
 
 class BaseCredential(BaseProviderObject):
 
