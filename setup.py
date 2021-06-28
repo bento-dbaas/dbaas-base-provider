8e49fba304ebfd5bf730649a4d6d84a2f85f80ee
Ricardo Dias <ricardo@ricardodias.org>
2021-06-28 13:07:52 -0300

    new version and new requirements

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from dbaas_base_provider.version import Version
 
 
 setuptools.setup(name='dbaas-base-provider',
-                 version=Version('0.0.7').number,
+                 version=Version('0.0.8').number,
                  description='Base for DBaaS providers',
                  long_description=open('README.md').read().strip(),
                  author='William Marquardt',
@@ -18,7 +18,8 @@ setuptools.setup(name='dbaas-base-provider',
                  py_modules=['dbaas_base_provider'],
                  install_requires=[
                     'mongoengine==0.15.0',
-                    'pymongo==3.6.1'
+                    'pymongo==3.6.1',
+                    'python-slugify==4.0.1'
                  ],
                  license='BSD 3-Clause License',
                  zip_safe=False,
