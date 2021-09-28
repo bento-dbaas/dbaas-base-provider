d234d27485c14c3ace504d04a36c80f8bfa2cc94
Gabriel Bustos <gtbustos@gmail.com>
2021-09-28 10:56:11 -0300

    New version. Trying compatibility with Python 2 and 3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from dbaas_base_provider.version import Version
 
 
 setuptools.setup(name='dbaas-base-provider',
-                 version=Version('0.0.13').number,
+                 version=Version('0.0.14').number,
                  description='Base for DBaaS providers',
                  long_description=open('README.md').read().strip(),
                  author='William Marquardt',
@@ -17,7 +17,7 @@ setuptools.setup(name='dbaas-base-provider',
                  url='https://github.com/bento-dbaas/dbaas-base-provider',
                  py_modules=['dbaas_base_provider'],
                  install_requires=[
-                    'mongoengine>=0.23.1',
+                    'mongoengine>=0.15.0',
                     'pymongo==3.6.1',
                     'python-slugify==4.0.1'
                  ],
