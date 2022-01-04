0f84af2cb70fdfae9233c79a62ec5db57822f5ce
Marcio Mendonça <marcio.mendonca@gmail.com>
2022-01-04 17:58:07 -0300

    Basic Unit test for log_this decorator (Later, refine the test)

---
+++ tests/test_log/test_log.py
@@ -0,0 +1,16 @@
+from dbaas_base_provider.log import log_this
+from unittest import TestCase
+
+
+class TestLog(TestCase):
+
+    def test_logx(self):
+        @log_this
+        def f_teste(x,y):
+            try:
+                return x/y 
+            except Exception as e:
+                return "Erro"
+    
+        self.assertEqual(f_teste(10,5), 10/5, "Funçao executada sem erro")
+        self.assertEqual(f_teste(10,0), "Erro", "Funçao executada com erro")
